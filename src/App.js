023ca397740c07ebf5a2a7328c93448c3193d20a
MrBo88 <Mr@Bo88.io>
2020-12-02 21:20:09 +0300

    _servak

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
-import React from "react";
+import React, { useEffect } from "react";
 import TodoList from "./TodoList/TodoList";
-
-let todos = [
-	{id: 1, completed: false, title: 'Buy fruits'},
-	{id: 2, completed: false, title: 'Buy vegetables'},
-	{id: 3, completed: false, title: 'Buy beverages'}
-]
-	
+import Context from "./context";
+import AddTodo from "./TodoList/AddTodo";
+import { Preloader } from "./Preloader";
 
 function App() {
+  const [todos, setTodos] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState([true]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then((response) => response.json())
+      .then((todos) => {
+        setTimeout(() => {
+					setTodos(todos);
+					setIsLoading(false)
+        }, 2000);
+      });
+  }, []);
+
+  function changeTodo(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
+  }
+
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const addTodoItem = (title) => {
+    setTodos(
+      todos.concat({
+        id: Date.now(),
+        completed: false,
+        title,
+      })
+    );
+  };
+
   return (
-    <div className="wrapper">
-      <h1>React Tutorial</h1>
-      <TodoList todos={todos} />
-    </div>
+    <Context.Provider value={{ removeTodo }}>
+      <div className='wrapper'>
+        <h1>React Tutorial</h1>
+        <AddTodo onCreate={addTodoItem} />
+        {isLoading && <Preloader />}
+        {todos.length ? (
+          <TodoList todos={todos} onChange={changeTodo} />
+        ) : isLoading ? null : (
+          <b>Empty list</b>
+        )}
+      </div>
+    </Context.Provider>
   );
 }
 
--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -1,29 +1,37 @@
-import React from 'react';
-import { uniqueKeyConstructor } from '../helpers/uniqueKeyCreator';
-import TodoItem from './TodoItem';
-import PropTypes from 'prop-types';
+import React from "react";
+import { uniqueKeyConstructor } from "../helpers/uniqueKeyCreator";
+import TodoItem from "./TodoItem";
+import PropTypes from "prop-types";
 
 const styles = {
-	ul: {
-		listStyle: 'none',
-		margin: 0,
-		padding: 0
-	}
-}
+  ul: {
+    listStyle: "none",
+    margin: 0,
+    padding: 0,
+  },
+};
 
 const TodoList = (props) => {
-	return (
-		<ul style={styles.ul}>
-			{props.todos.map((todo, index) => {
-				return <TodoItem key={uniqueKeyConstructor()} todo={todo} index={index} />
-				})}
-			
-		</ul>
-	)
-}
+	console.log(props.todos);
+  return (
+    <ul style={styles.ul}>
+      {props.todos.map((todo, index) => {
+        return (
+          <TodoItem
+            key={uniqueKeyConstructor()}
+            todo={todo}
+            index={index}
+            onChange={props.onChange}
+          />
+        );
+      })}
+    </ul>
+  );
+};
 
 TodoList.propTypes = {
-	todos: PropTypes.arrayOf(PropTypes.object).isRequired
-}
+  todos: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onChange: PropTypes.func.isRequired,
+};
 
-export default TodoList	
+export default TodoList;
